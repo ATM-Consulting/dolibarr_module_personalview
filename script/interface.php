ac5d9f6a9c1923417128e2737de0160a8b2f2667
jpb <jean-pascal.boudet@atm-consulting>
2021-10-26 11:16:12 +0200

    fix token

--- script/interface.php
+++ script/interface.php
@@ -1,29 +1,29 @@
 <?php
-
+if (!defined('NOTOKENRENEWAL')) define('NOTOKENRENEWAL', 1); // Disables token renewal
 	require '../config.php';
-	
+
 	dol_include_once('/personalview/class/ps.class.php');
 
 		$put = GETPOST('put');
-		
+
 		_put($put);
-		
+
 function _put($put) {
 	$PDOdb = new TPDOdb;
-	
+
 	switch ($put) {
 		case 'view':
 			$ps =new TPersonalView;
 			$ps->loadByElementAction($PDOdb, GETPOST('element'),  GETPOST('action'));
-			
+
 			$ps->element = GETPOST('element');
 			$ps->action = GETPOST('action');
 			$ps->TField = GETPOST('TField','array');
 			pre($ps->TField,1);
 			echo $ps->save($PDOdb);
-			
+
 			break;
-		
+
 	}
-	
+
 }
